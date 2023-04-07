fc3474ef38f013654da7d06a4fc3e1ed7e9c371c
Keara Harris <keara.s.harris@gmail.com>
2023-04-07 00:25:43 -0500

    Completed phase1 functions lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(locationPickUp) {
+    const endLocation = 42;
+    let amtBlocks = Math.abs(locationPickUp - endLocation);
+    return amtBlocks
+
+}
+
+function distanceFromHqInFeet(locationPickUp){
+    return distanceFromHqInBlocks(locationPickUp) * 264
+
+}
+
+function distanceTravelledInFeet(locationPickUp, endLocation){
+    let pickUpInFeet = locationPickUp * 264;
+    let endLocationInFeet = endLocation * 264
+    return Math.abs(pickUpInFeet - endLocationInFeet)
+    return blocksToFeet;
+}
+
+function calculatesFarePrice(locationPickUp, endLocation){
+    
+   let distanceFeet = distanceTravelledInFeet(locationPickUp, endLocation);
+   let fare;
+   let fareWithoutRounding;
+    if (distanceFeet <= 400){
+        fare = 0;
+    }else if (distanceFeet > 400 && distanceFeet < 2000){
+        fareWithoutRounding = ((distanceFeet * .02)-(.02*400))
+        fare = parseFloat(fareWithoutRounding.toFixed(2));
+        
+    }else if (distanceFeet > 2000 && distanceFeet < 2500){
+        fare = 25;
+
+    }else if (distanceFeet >= 2500){
+        fare = "cannot travel that far";
+
+    }
+    return fare
+}
